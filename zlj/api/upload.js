4bbe8f5e96d78505643ca0f100caeddcc465ebd5
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2019-12-13 13:30:30 +0800

    editor upload image

--- zlj/api/upload.js
+++ zlj/api/upload.js
@@ -8,8 +8,9 @@ function uploadimage (req, res) {
       var images = keystone.list( "Imageupload" ) //Imageupload
       item = images.model()
 
+
       // ckeditor ajax is "upload", but Imageupload model fields is "image"
-      req.files.image = req.files.upload
+      //req.files.image = req.files.upload
 
       images.updateItem(item,req.body,{files:req.files},function(err){
           if (err){
@@ -21,7 +22,10 @@ function uploadimage (req, res) {
 
           // return for ctrl+v , clipboard
           if (req.query['responseType'] == 'json'){
-                return res.json({"fileName":"image.png","uploaded":1,"url":filename})
+                //return res.json({"fileName":"image.png","uploaded":1,"url":filename})
+                return res.json({"errno": 0,"data": [
+                    filename
+                ]})
           } else {
             // return for upload file by form submit
             res.setHeader('Content-Type', 'text/html');
